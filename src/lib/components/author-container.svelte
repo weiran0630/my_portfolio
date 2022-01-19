6f4e78308bdc1685921661b05c57b8e56e3ed2a5
Wei-ran <wei-ran@tuta.io>
2022-01-19 18:23:35 +0800

    bio with markdown

--- src/lib/components/author-container.svelte
+++ src/lib/components/author-container.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { Author } from '$lib/models/author';
+	import SvelteMarkdown from 'svelte-markdown';
 
 	export let authors: Author[] = [];
 	const {
@@ -12,13 +13,13 @@
 <div class="mb-24 flex flex-col select-none">
 	<h1 class="font-bold text-center mb-20 mt-10 text-5xl">Welcome to my Portfolio 😆</h1>
 
-	<div class="flex flex-wrap md:flex-nowrap  justify-center items-center md:justify-start">
-		<img class="mask mask-circle h-48 mb-10 md:ml-5" src={url} alt={name} />
+	<div class="flex flex-wrap md:flex-nowrap justify-center items-center md:justify-start">
+		<img class="mask mask-circle h-48 md:ml-5" src={url} alt={name} />
 
 		<div class="ml-7">
 			<h2 class="text-3xl mb-4 font-bold tracking-wider">{name}</h2>
 
-			<p class="text-xl mb-4">{@html intro}</p>
+			<SvelteMarkdown source={intro} />
 		</div>
 	</div>
 </div>
