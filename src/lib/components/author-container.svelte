f2f821b83946b883f4973d972c6ce6770ce3c7c9
Wei-ran <wei-ran@tuta.io>
2022-02-14 17:02:40 +0800

    animated iam fix

--- src/lib/components/author-container.svelte
+++ src/lib/components/author-container.svelte
@@ -23,19 +23,19 @@
 </script>
 
 <div class="mb-48 flex flex-col select-none">
-	<h1 class="font-bold text-center mb-20 mt-10  text-5xl">Welcome to my Portfolio 😆</h1>
+	<h1 class="font-bold text-center mb-20 mt-10 text-5xl">Welcome to my Portfolio 😆</h1>
 
 	<div class="flex flex-wrap md:flex-nowrap justify-center items-center md:justify-start">
 		<img class="mask mask-circle h-48 md:ml-5 mb-10 sm:mb-0" src={url} alt={name} />
 
 		<div class="ml-7">
-			<h2 class="text-3xl mb-4 font-bold tracking-wider">
+			<h2 class="text-3xl mt-4 sm:mb-4 font-bold tracking-wider min-h-[73px] sm:min-h-fit ">
 				Hi! I'm
 				{#key whoami}
 					<span
-						class="text-primary inline-block font-mono "
-						in:fly={{ y: -10, delay: 500, duration: 500 }}
-						out:fly={{ y: 10, duration: 500 }}>{whoami}</span
+						class="text-primary inline-block font-mono"
+						in:fly={{ y: -20, delay: 300 }}
+						out:fly={{ y: 20, duration: 300 }}>{whoami}</span
 					>
 				{/key}
 			</h2>
