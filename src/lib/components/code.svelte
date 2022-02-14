0a314bf1431ce67758ad89b84e4b5a9d0c3598d2
Wei-ran <wei-ran@tuta.io>
2022-02-14 18:34:18 +0800

    copy to clipboard

--- src/lib/components/code.svelte
+++ src/lib/components/code.svelte
@@ -1,12 +1,33 @@
 <script lang="ts">
 	import { HighlightSvelte } from 'svelte-highlight';
 	import style from 'svelte-highlight/src/styles/atom-one-dark';
+	import Fa from 'svelte-fa/src/fa.svelte';
+	import { faCopy } from '@fortawesome/free-solid-svg-icons';
 
 	export let text: string;
+	export let copied: boolean = false;
+
+	function copyToClipboard() {
+		navigator.clipboard.writeText(text);
+		copied = true;
+
+		setTimeout(() => {
+			copied = false;
+		}, 1000);
+	}
 </script>
 
 <svelte:head>
 	{@html style}
 </svelte:head>
 
-<HighlightSvelte class="mockup-code !pl-3 pt-7 !select-text" code={text} />
+<div class="mockup-code !select-text relative">
+	<span
+		on:click={copyToClipboard}
+		data-tip={copied ? `Copied!` : `Copy to clipboard`}
+		class="opacity-25 hover:opacity-100 absolute right-[24px] hover:cursor-pointer transition tooltip tooltip-left"
+	>
+		<Fa icon={faCopy} />
+	</span>
+	<HighlightSvelte class="m-0 py-0 max-h-fit" code={text} />
+</div>
