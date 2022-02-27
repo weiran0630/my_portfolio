a4be53940888386889da539f902ae443b9aa3597
Wei-ran <wei-ran@tuta.io>
2022-02-28 02:26:08 +0800

    theme update

--- src/lib/components/theme-switcher.svelte
+++ src/lib/components/theme-switcher.svelte
@@ -1,13 +1,13 @@
 <script>
 	import { browser } from '$app/env';
 
-	const defaultValue = 'cupcake';
+	const defaultValue = 'emerald';
 	const initialValue = browser ? window.localStorage.getItem('theme') : defaultValue;
 </script>
 
 <div class="flex gap-1 mt-1">
 	<span class="label-text text-lg pb-2">☀️</span>
-	<button data-toggle-theme="cupcake,synthwave" data-act-class="ACTIVECLASS">
+	<button data-toggle-theme="emerald,synthwave" data-act-class="ACTIVECLASS">
 		<input
 			type="checkbox"
 			class="toggle toggle-primary"
