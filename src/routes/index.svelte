0ec59fdf45c155feba73c4454e00b35bd184126b
Wei-ran <wei-ran@tuta.io>
2022-03-18 11:50:24 +0800

    remove ad sense

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -26,7 +26,6 @@
 	import AuthorContainer from '$lib/components/author-container.svelte';
 	import TechStacks from '$lib/components/tech-stacks.svelte';
 	import { fade } from 'svelte/transition';
-	import Adsense from '$lib/components/adsense.svelte';
 
 	export let projects: Project[];
 	export let authors: Author[];
