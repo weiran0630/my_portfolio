89cf2f6a1fc3bc19674878a5185ce62f9dfd28c4
Edison.ec.suiqui <ecweiran@suiqui.com>
2022-12-29 21:42:03 +0800

    feat(lib/components/work-exp-container.svelte) create work-exp-container component

---
+++ src/lib/components/work-exp-container.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import type { WorkExperience } from '$lib/models/workExperience';
+
+	export let workExperiences: WorkExperience[];
+</script>
+
+<h1 class="text-5xl font-bold text-center mb-10">Work Experiences 💼</h1>
+
+<div class="mb-48">
+	{#each workExperiences as { company, jobTitle, image, description }}
+		<div class="card card-side bg-base-100 flex">
+			<figure class="w-2">
+				<img class="mask mask-circle h-48 md:ml-5 mb-10 sm:mb-0" src={image.url} alt={jobTitle} />
+			</figure>
+			<div class="card-body">
+				<h2
+					class="card-title text-primary text-3xl mt-4 sm:mb-4 font-bold tracking-wider min-h-[73px] sm:min-h-fit"
+				>
+					{company} - {jobTitle}
+				</h2>
+				<p>{description}</p>
+			</div>
+		</div>
+	{/each}
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,19 +4,25 @@
 	import type { Project } from '$lib/models/projects';
 	import type { Author } from '$lib/models/author';
 	import type { Tech } from '$lib/models/tech';
+	import type { WorkExperience } from '$lib/models/workExperience';
 	import { client } from '$lib/shared/graphql-client';
-	import { authorsQuery, TechQuery, projectsQuery } from '$lib/shared/queries';
+	import { authorsQuery, TechQuery, projectsQuery, WorkExperienceQuery } from '$lib/shared/queries';
 
 	export const load: Load = async () => {
-		const [{ authors }, { projects }, { frontendTeches, backendTeches, miscTeches }] =
-			await Promise.all([
-				client.request(authorsQuery),
-				client.request(projectsQuery),
-				client.request(TechQuery)
-			]);
+		const [
+			{ authors },
+			{ projects },
+			{ frontendTeches, backendTeches, miscTeches },
+      { workExperiences }
+		] = await Promise.all([
+			client.request(authorsQuery),
+			client.request(projectsQuery),
+			client.request(TechQuery),
+			client.request(WorkExperienceQuery)
+		]);
 
 		return {
-			props: { projects, authors, frontendTeches, backendTeches, miscTeches }
+			props: { projects, authors, frontendTeches, backendTeches, miscTeches, workExperiences }
 		};
 	};
 </script>
@@ -24,6 +30,7 @@
 <script lang="ts">
 	import ProjectContainer from '$lib/components/project-container.svelte';
 	import AuthorContainer from '$lib/components/author-container.svelte';
+	import WorkExpContainer from '$lib/components/work-exp-container.svelte';
 	import TechStacks from '$lib/components/tech-stacks.svelte';
 	import { fade } from 'svelte/transition';
 
@@ -32,6 +39,7 @@
 	export let frontendTeches: Tech[];
 	export let backendTeches: Tech[];
 	export let miscTeches: Tech[];
+	export let workExperiences: WorkExperience[];
 </script>
 
 <svelte:head>
@@ -42,6 +50,8 @@
 	<AuthorContainer {authors} />
 
 	<TechStacks {frontendTeches} {backendTeches} {miscTeches} />
+
+	<WorkExpContainer {workExperiences} />
 
 	<div lang="zh-Hant-TW">
 		<ProjectContainer {projects} />
